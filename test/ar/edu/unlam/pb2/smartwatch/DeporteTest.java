887eb7dca2d0653148455df7522dd60ee758d64e
Gaby137 <genrriquezsanchez@alumno.unlam.edu.ar>
2021-10-06 09:33:19 -0300

    se sube el proyecto inicial

---
+++ test/ar/edu/unlam/pb2/smartwatch/DeporteTest.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.pb2.smartwatch;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+class DeporteTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
