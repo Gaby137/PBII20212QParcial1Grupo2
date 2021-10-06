762cf66ac0758cce6207569b6cf4f63366a39184
Gaby137 <genrriquezsanchez@alumno.unlam.edu.ar>
2021-10-06 09:44:18 -0300

    se cambia el nombre de deportista

--- src/ar/edu/unlam/pb2/smartwatch/Deportista.java
+++ src/ar/edu/unlam/pb2/smartwatch/Deportista.java
@@ -1,5 +1,5 @@
 package ar.edu.unlam.pb2.smartwatch;
 
-public class Deporte {
+public class Deportista {
 
 }
--- test/ar/edu/unlam/pb2/smartwatch/DeportistaTest.java
+++ test/ar/edu/unlam/pb2/smartwatch/DeportistaTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
-class DeporteTest {
+class DeportistaTest {
 
 	@Test
 	void test() {
