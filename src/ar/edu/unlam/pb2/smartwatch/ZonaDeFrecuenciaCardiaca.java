1377f979dff5e2c53763580c9fe1a2e7d14a3905
Celeste <celgimenez@alumno.unlam.edu.ar>
2021-10-06 12:02:27 -0300

    se crearon las clases ZonaDeFrecuenciaCardiaca y TipoDeZona

---
+++ src/ar/edu/unlam/pb2/smartwatch/ZonaDeFrecuenciaCardiaca.java
@@ -0,0 +1,17 @@
+package ar.edu.unlam.pb2.smartwatch;
+
+public class ZonaDeFrecuenciaCardiaca {
+	private TipoDeZona tipoZona;
+
+	public TipoDeZona getTipoZona() {
+		return tipoZona;
+	}
+
+	public void setTipoZona(TipoDeZona tipoZona) {
+		this.tipoZona = tipoZona;
+	}
+
+	public Double calcularFrecuenciaCardiacaActual() {
+		return 0.0;
+	}
+}
