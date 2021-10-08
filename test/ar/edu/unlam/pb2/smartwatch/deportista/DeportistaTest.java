7d8995b4d665c1b82ac59148f74b4a3c42b28c3b
Gaby137 <genrriquezsanchez@alumno.unlam.edu.ar>
2021-10-08 13:56:13 -0300

    se mergea con los cambios de produccion y se corrige un import

--- test/ar/edu/unlam/pb2/smartwatch/deportista/DeportistaTest.java
+++ test/ar/edu/unlam/pb2/smartwatch/deportista/DeportistaTest.java
@@ -1,10 +1,11 @@
 package ar.edu.unlam.pb2.smartwatch.deportista;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 import ar.edu.unlam.pb2.smartwatch.sensor.Paso;
+import ar.edu.unlam.pb2.smartwatch.sensor.Pedaleo;
 
 public class DeportistaTest {
 
@@ -41,15 +42,16 @@ public class DeportistaTest {
 		final Integer EDAD_ESPERADA = 35;
 		final Double PESO_ESPERADO = 75.80;
 		final Double ALTURA_ESPERADA = 177.4;
-		
+
 		Ciclista ciclista = new Ciclista(NOMBRE_ESPERADO, EDAD_ESPERADA, PESO_ESPERADO, ALTURA_ESPERADA);
 
 		assertEquals(NOMBRE_ESPERADO, ciclista.getNombre());
-		assertEquals(EDAD_ESPERADA, ciclista.getEdadActual(),0.0);
-		assertEquals(PESO_ESPERADO, ciclista.getPesoActual(),0.0);
-		assertEquals(ALTURA_ESPERADA, ciclista.getAlturaActual(),0.0);
+		assertEquals(EDAD_ESPERADA, ciclista.getEdadActual(), 0.0);
+		assertEquals(PESO_ESPERADO, ciclista.getPesoActual(), 0.0);
+		assertEquals(ALTURA_ESPERADA, ciclista.getAlturaActual(), 0.0);
 
 	}
+
 	@Test
 	public void queSePuedaInstanciarUnNadador() {
 
@@ -57,15 +59,16 @@ public class DeportistaTest {
 		final Integer EDAD_ESPERADA = 30;
 		final Double PESO_ESPERADO = 75.80;
 		final Double ALTURA_ESPERADA = 177.4;
-		
+
 		Nadador nadador = new Nadador(NOMBRE_ESPERADO, EDAD_ESPERADA, PESO_ESPERADO, ALTURA_ESPERADA);
 
 		assertEquals(NOMBRE_ESPERADO, nadador.getNombre());
-		assertEquals(EDAD_ESPERADA, nadador.getEdadActual(),0.0);
-		assertEquals(PESO_ESPERADO, nadador.getPesoActual(),0.0);
-		assertEquals(ALTURA_ESPERADA, nadador.getAlturaActual(),0.0);
+		assertEquals(EDAD_ESPERADA, nadador.getEdadActual(), 0.0);
+		assertEquals(PESO_ESPERADO, nadador.getPesoActual(), 0.0);
+		assertEquals(ALTURA_ESPERADA, nadador.getAlturaActual(), 0.0);
 
 	}
+
 	@Test
 	public void queSePuedaInstanciarUnRunner() {
 
@@ -73,15 +76,16 @@ public class DeportistaTest {
 		final Integer EDAD_ESPERADA = 35;
 		final Double PESO_ESPERADO = 75.80;
 		final Double ALTURA_ESPERADA = 177.4;
-		
+
 		Runner runner = new Runner(NOMBRE_ESPERADO, EDAD_ESPERADA, PESO_ESPERADO, ALTURA_ESPERADA);
 
 		assertEquals(NOMBRE_ESPERADO, runner.getNombre());
-		assertEquals(EDAD_ESPERADA, runner.getEdadActual(),0.0);
-		assertEquals(PESO_ESPERADO, runner.getPesoActual(),0.0);
-		assertEquals(ALTURA_ESPERADA, runner.getAlturaActual(),0.0);
+		assertEquals(EDAD_ESPERADA, runner.getEdadActual(), 0.0);
+		assertEquals(PESO_ESPERADO, runner.getPesoActual(), 0.0);
+		assertEquals(ALTURA_ESPERADA, runner.getAlturaActual(), 0.0);
 
 	}
+
 	@Test
 	public void queSePuedaInstanciarUnCaminador() {
 
@@ -89,31 +93,32 @@ public class DeportistaTest {
 		final Integer EDAD_ESPERADA = 35;
 		final Double PESO_ESPERADO = 75.80;
 		final Double ALTURA_ESPERADA = 177.4;
-		
+
 		Caminador caminador = new Caminador(NOMBRE_ESPERADO, EDAD_ESPERADA, PESO_ESPERADO, ALTURA_ESPERADA);
 
 		assertEquals(NOMBRE_ESPERADO, caminador.getNombre());
-		assertEquals(EDAD_ESPERADA, caminador.getEdadActual(),0.0);
-		assertEquals(PESO_ESPERADO, caminador.getPesoActual(),0.0);
-		assertEquals(ALTURA_ESPERADA, caminador.getAlturaActual(),0.0);
+		assertEquals(EDAD_ESPERADA, caminador.getEdadActual(), 0.0);
+		assertEquals(PESO_ESPERADO, caminador.getPesoActual(), 0.0);
+		assertEquals(ALTURA_ESPERADA, caminador.getAlturaActual(), 0.0);
 
 	}
+
 	@Test
 	public void queSePuedaConocerLaDistanciaRecorridaDeUnCiclista() {
-		final Integer PEDALEOS=20;
-		final Double PEDALEOS_KM=Pedaleo.LONGITUD_DE_PEDALEO_PROM/1000;
-		final Double VALOR_ESPERADO=0.0178;
-		
-		Ciclista ciclista=new Ciclista("Lance",35,75.80,177.4);
-		
-		Pedaleo p1=new Pedaleo(100);
-		
+		final Integer PEDALEOS = 20;
+		final Double PEDALEOS_KM = Pedaleo.LONGITUD_DE_PEDALEO_PROM / 1000;
+		final Double VALOR_ESPERADO = 0.0178;
+
+		Ciclista ciclista = new Ciclista("Lance", 35, 75.80, 177.4);
+
+		Pedaleo p1 = new Pedaleo(100);
+
 		for (int i = 0; i < PEDALEOS; i++) {
 			p1.darUnPedaleo();
 		}
-		Double valorObtenido=ciclista.distanciaRecorrida(p1.getCantDePedaleo(), PEDALEOS_KM);
-		
-		assertEquals(VALOR_ESPERADO,valorObtenido);
+		Double valorObtenido = ciclista.distanciaRecorrida(p1.getCantDePedaleo(), PEDALEOS_KM);
+
+		assertEquals(VALOR_ESPERADO, valorObtenido);
 	}
 
 }
